3655107fe95d5d1a08e72ceb5fd43f3f0250dbe2
William Eduardo Meza <williameduardomeza@gmail.com>
2020-06-11 08:59:30 -0500

    feat: initial commit and base gameplay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const [state, setState] = React.useState(
+    {
+      isPlayerOne: true,
+    }
+  )
+  const [matrix, setMatrix] = React.useState([
+    ['','',''], 
+    ['','',''], 
+    ['','', '']
+  ]);
+
+  const handleClick = (i, j) => {
+    if(matrix[i][j]){
+      return;
+    }
+    const newMatrix = matrix;
+    if(state.isPlayerOne){
+      newMatrix[i][j] = 'X';
+    } else {
+      newMatrix[i][j] = 'O';
+    }
+    setState({
+      isPlayerOne: !state.isPlayerOne
+    })
+    setMatrix(newMatrix)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      <div class="status">
+        Next player: {state.isPlayerOne ? 'X' : 'O'}
+      </div>
+      <div class="board">
+        {
+          matrix.map((row, i) => (
+            <div className="board-row">
+              {row.map((item,j) => (
+                <button className="square" onClick={() => handleClick(i,j)}>
+                  {item}
+                </button>
+              ))}
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
